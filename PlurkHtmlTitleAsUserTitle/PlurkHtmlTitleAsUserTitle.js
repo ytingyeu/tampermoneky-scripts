d83e636a4c168c6046c037609d2d862b83fb3f0f
Tingyeu Yang <ytingyeu@asu.edu>
2021-08-20 13:59:48 -0700

    refactor to use onload event

--- PlurkHtmlTitleAsUserTitle/PlurkHtmlTitleAsUserTitle.js
+++ PlurkHtmlTitleAsUserTitle/PlurkHtmlTitleAsUserTitle.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PlurkHtmlTitleAsUserTitle
 // @namespace    https://github.com/ytingyeu
-// @version      0.1
+// @version      0.2
 // @description  Replace HTML title with user page title
 // @author       Tingyeu Yang <ytingyeu@asu.edu>
 // @include      *://www.plurk.com/*
@@ -13,11 +13,12 @@
 (function () {
   "use strict";
 
-  if (document.readyState == "interactive") {
+  function onLoad() {
     let userTitleElement = document.getElementById("page_title");
-
     if (userTitleElement !== null) {
       document.title = userTitleElement.innerText;
     }
   }
+
+  window.addEventListener("load", onLoad);
 })();
